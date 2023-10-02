032dfe7f9898293baf02504126b713c1ae6892a1
teoCV333 <teo333>
2023-10-02 16:34:39 -0500

    launch camera added

--- app/src/main/java/com/example/android_basic_app/MainActivity.java
+++ app/src/main/java/com/example/android_basic_app/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.android_basic_app;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnGoogle, btnAlarm, btnCall;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Button btnGoogle, btnAlarm, btnCall, btnCamera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btnGoogle = findViewById(R.id.btnGoogle);
         btnAlarm = findViewById(R.id.btnAlarm);
         btnCall = findViewById(R.id.btnCall);
+        btnCamera = findViewById(R.id.btnCamera);
 
         btnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,16 +56,33 @@ public class MainActivity extends AppCompatActivity {
         btnCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String phoneNumber = "3243888917";
                 Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:" + 324388891));
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                Object res = intent.resolveActivity(getPackageManager());
+                startActivity(intent);
             }
         });
 
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    camLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+                } catch (Error error) {
+                    Log.d(error.toString(), "error camera: ");
+                }
+            }
+        });
     }
-    private void addAlarm() {
 
-    }
+    ActivityResultLauncher<Intent> camLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == RESULT_OK) {
+                Bundle extras = result.getData().getExtras();
+                Bitmap imgBitmap = (Bitmap) extras.get("data");
+            }
+        }
+    });
 }
